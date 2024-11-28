55ef457b56cce8737ce30405e8409d182b8007ab
Roberto Loza <76535183+Loza64@users.noreply.github.com>
2024-11-28 13:04:21 -0600

    changes

--- Web/src/components/history.tsx
+++ Web/src/components/history.tsx
@@ -1,14 +1,39 @@
 import React from "react";
 import { Consumer } from "../context/Context";
-import Card from "./card";
 
 const History: React.FC = () => {
     const { history } = Consumer();
+
+    const list = history.sort((a, b) => {
+        const dateTimeA = new Date(`${a.date} ${a.time}`);
+        const dateTimeB = new Date(`${b.date} ${b.time}`);
+        return dateTimeB.getTime() - dateTimeA.getTime();
+    });
+
     return (
         <div className="history">
-            {
-                history.sort().map(({ date, time, type, details }, index) => <Card key={index} date={date} time={time} type={type} details={details} />)
-            }
+            <table>
+                <thead>
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Hora</th>
+                        <th>Tipo</th>
+                        <th>Detalles</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        list.map(({ date, time, type, details }, index) => (
+                            <tr style={{ background: (index + 1) % 2 === 0 ? "white" : "#EEEEEE" }} role="row" aria-label={`Event on ${date} at ${time}`}>
+                                <td>{date}</td>
+                                <td>{time}</td>
+                                <td>{type}</td>
+                                <td>{details}</td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>
         </div>
     );
 }
